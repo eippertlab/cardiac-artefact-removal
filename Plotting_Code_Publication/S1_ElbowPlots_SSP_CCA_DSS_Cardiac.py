501aa0428a8804f287442a5cd699898a29c25fae
Emma Bailey <bailey@cbs.mpg.de>
2025-02-10 16:57:39 +0100

    Update Images

--- Plotting_Code_Publication/S1_ElbowPlots_SSP_CCA_DSS_Cardiac.py
+++ Plotting_Code_Publication/S1_ElbowPlots_SSP_CCA_DSS_Cardiac.py
@@ -8,12 +8,18 @@ from math import log10
 from kneed import KneeLocator
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
+import os
+mpl.rcParams['pdf.fonttype'] = 42
 
 if __name__ == '__main__':
     input_paths = {'SSP': "/data/pt_02569/tmp_data/ssp_py/",
                    'CCA_heart': "/data/pt_02569/tmp_data/cca_heartart_py/",
                    'DSS_heart': "/data/pt_02569/tmp_data/dss_heartart_py/"
                    }
+    image_path = "/data/p_02569/Images/ElbowPlots/"
+    os.makedirs(image_path, exist_ok=True)
+
     esg_chans = ['S35', 'S24', 'S36', 'Iz', 'S17', 'S15', 'S32', 'S22',
                  'S19', 'S26', 'S28', 'S9', 'S13', 'S11', 'S7', 'SC1', 'S4', 'S18',
                  'S8', 'S31', 'SC6', 'S12', 'S16', 'S5', 'S30', 'S20', 'S34', 'AC',
@@ -93,60 +99,64 @@ if __name__ == '__main__':
             residual_tib.append((np.mean(res_tib[:, tibial_pos] / res_tib_prep[:, tibial_pos], axis=tuple([0, 1]))) * 100)
 
         # Images
-        fig, axes = plt.subplots(1, 3)
+        fig, axes = plt.subplots(1, 3, figsize=[18, 6])
         axes = axes.flatten()
         plt.suptitle(f"{method}, median")
         axes[0].scatter(projectors, snr_med)
         axes[0].axvline(list(snr_med).index(np.max(snr_med))+1, color='red', label=f"{list(snr_med).index(np.max(snr_med))+1}")
         axes[0].legend()
-        axes[0].set_xlabel('No. Projectors')
-        axes[0].set_ylabel('Value')
-        axes[0].set_title('SNR')
+        axes[0].set_xlabel('Number of Projectors')
+        axes[0].set_ylabel('Signal-to-Noise Ratio (AU)')
+        axes[0].set_title('Signal-to-Noise Ratio')
         axes[0].set_xticks(projectors)
         axes[1].scatter(projectors, residual_med)
         kn = KneeLocator(projectors, residual_med, curve='convex', direction='decreasing')
         axes[1].axvline(kn.knee, color='red', label=f"{kn.knee}")
         axes[1].legend()
-        axes[1].set_xlabel('No. Projectors')
-        axes[1].set_ylabel('Value')
-        axes[1].set_title('RI')
+        axes[1].set_xlabel('Number of Projectors')
+        axes[1].set_ylabel('Residual Intensity (%)')
+        axes[1].set_title('Residual Intensity')
         axes[1].set_xticks(projectors)
         axes[2].scatter(projectors, inps_med)
         kn = KneeLocator(projectors, inps_med, curve='concave', direction='increasing')
         axes[2].axvline(kn.knee, color='red', label=f"{kn.knee}")
         axes[2].legend()
-        axes[2].set_xlabel('No. Projectors')
-        axes[2].set_ylabel('Log Value')
-        axes[2].set_title('INPSR')
+        axes[2].set_xlabel('Number of Projectors')
+        axes[2].set_ylabel('Log(INPSR) (AU)')
+        axes[2].set_title('Improved Normalised Power Spectrum Ratio')
         axes[2].set_xticks(projectors)
         plt.tight_layout()
+        plt.savefig(image_path + f"{method}_median.png")
+        plt.savefig(image_path + f"{method}_median" + '.pdf', bbox_inches='tight', format="pdf")
 
-        fig, axes = plt.subplots(1, 3)
+        fig, axes = plt.subplots(1, 3, figsize=[18, 6])
         axes = axes.flatten()
         plt.suptitle(f"{method}, tibial")
         axes[0].scatter(projectors, snr_tib)
         axes[0].axvline(list(snr_tib).index(np.max(snr_tib)) + 1, color='red', label=f"{list(snr_tib).index(np.max(snr_tib)) + 1}")
         axes[0].legend()
-        axes[0].set_xlabel('No. Projectors')
-        axes[0].set_ylabel('Value')
-        axes[0].set_title('SNR')
+        axes[0].set_xlabel('Number of Projectors')
+        axes[0].set_ylabel('Signal-to-Noise Ratio (AU)')
+        axes[0].set_title('Signal-to-Noise Ratio')
         axes[0].set_xticks(projectors)
         axes[1].scatter(projectors, residual_tib)
         kn = KneeLocator(projectors, residual_tib, curve='convex', direction='decreasing')
         axes[1].axvline(kn.knee, color='red', label=f"{kn.knee}")
         axes[1].legend()
-        axes[1].set_xlabel('No. Projectors')
-        axes[1].set_ylabel('Value')
-        axes[1].set_title('RI')
+        axes[1].set_xlabel('Number of Projectors')
+        axes[1].set_ylabel('RI (%)')
+        axes[1].set_title('Residual Intensity')
         axes[1].set_xticks(projectors)
         axes[2].scatter(projectors, inps_tib)
         kn = KneeLocator(projectors, inps_tib, curve='concave', direction='increasing')
         axes[2].axvline(kn.knee, color='red', label=f"{kn.knee}")
         axes[2].legend()
-        axes[2].set_xlabel('No. Projectors')
-        axes[2].set_ylabel('Log Value')
-        axes[2].set_title('INPSR')
+        axes[2].set_xlabel('Number of Projectors')
+        axes[2].set_ylabel('Log(INPSR) (AU)')
+        axes[2].set_title('Improved Normalised Power Spectrum Ratio')
         axes[2].set_xticks(projectors)
         plt.legend()
         plt.tight_layout()
-        plt.show()
+        plt.savefig(image_path + f"{method}_tibial.png")
+        plt.savefig(image_path + f"{method}_tibial" + '.pdf', bbox_inches='tight', format="pdf")
+        # plt.show()
